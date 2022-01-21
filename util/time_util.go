2788b25c2f0098ad991d026a920d7cd880b817e0
jzy <123456789>
2022-01-21 18:25:22 +0800

    time check

--- util/time_util.go
+++ util/time_util.go
@@ -15,3 +15,10 @@ func CheckTimeFormat(src, layout string) bool {
 	_, err := time.Parse(layout, src)
 	return err == nil
 }
+
+//获取零点时间
+func ZeroUnixTime(offsetDay int) int64 {
+	t := time.Now()
+	newTime := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+	return newTime.Unix()*1000 + int64(offsetDay*24*3600*1000)
+}
