7f799b61a4249a8078e6bf47be5bd4571f859bbc
jzy <123456789>
2022-02-23 15:49:35 +0800

    server time offset

--- util/time_util.go
+++ util/time_util.go
@@ -1,6 +1,38 @@
 package util
 
-import "time"
+import (
+	"github.com/jzyong/golib/log"
+	"time"
+)
+
+//自定义系统时间，可以内部设置偏移量
+
+//时间偏移量 秒
+var timeOffsetSecond int64 = 0
+
+//设置系统时间
+func SetTime(timeStr string) {
+	t, err := time.ParseInLocation("2006-01-02 15:04:05", timeStr, time.Local)
+	if err != nil {
+		log.Error("parse Time %v error:%v", timeStr, err)
+	}
+	timeOffsetSecond = t.Unix() - time.Now().Unix()
+}
+
+//获取 服务器内部时间戳s，可能有偏移量
+func CurrentTimeSecond() int64 {
+	return time.Now().Unix() + timeOffsetSecond
+}
+
+//获取 服务器内部时间戳 ms，可能有偏移量
+func CurrentTimeMillisecond() int64 {
+	return CurrentTimeSecond() * 1000
+}
+
+//当前时间，有时间偏移量
+func Now() time.Time {
+	return time.Unix(CurrentTimeSecond(), 0)
+}
 
 //是否为同一天
 func SameDay(a, b time.Time) bool {
@@ -16,14 +48,15 @@ func SameWeek(a, b int64) bool {
 
 //和当前时间是否为同一周  单位为s
 func SameWeekNow(a int64) bool {
-	return SameWeek(time.Now().Unix(), a)
+	return SameWeek(Now().Unix(), a)
 }
 
 //是否为今天
 func IsToday(ts int64) bool {
-	return SameDay(time.Unix(ts, 0), time.Now())
+	return SameDay(time.Unix(ts, 0), Now())
 }
 
+//检测时间格式是否正确
 func CheckTimeFormat(src, layout string) bool {
 	_, err := time.Parse(layout, src)
 	return err == nil
@@ -31,7 +64,7 @@ func CheckTimeFormat(src, layout string) bool {
 
 //获取零点时间
 func ZeroUnixTime(offsetDay int) int64 {
-	t := time.Now()
+	t := Now()
 	newTime := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 	return newTime.Unix()*1000 + int64(offsetDay*24*3600*1000)
 }
--- util/time_util_test.go
+++ util/time_util_test.go
@@ -3,15 +3,23 @@ package util
 import (
 	"fmt"
 	"testing"
+	"time"
 )
+
+//测试获取时间戳时间
+func TestCurrentTimeSecond(t *testing.T) {
+	SetTime("2022-02-23 16:00:00")
+	second := CurrentTimeSecond()
+	fmt.Printf("time Offset scond %v \n", second-time.Now().Unix())
+}
 
 //测试是否同一周
 func TestSameWeek(t *testing.T) {
-	//2022-1-28	2022-1-30
-	sameWeek := SameWeekNow(1643555224)
+	//2022-1-28	2022-1-30 true
+	sameWeek := SameWeek(1643317200, 1643490000)
 	fmt.Printf("same week:%v\n", sameWeek)
 
-	//2022-1-28	2022-1-31
-	sameWeek = SameWeekNow(1643641624)
+	//2022-1-28	2022-1-31 false false
+	sameWeek = SameWeek(1643317200, 1643641624)
 	fmt.Printf("same week:%v\n", sameWeek)
 }
